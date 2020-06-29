ff43b47bf8dccb442515a660b7c413d01d329524
ShebangDog <e1n18088@oit.ac.jp>
2020-06-29 21:34:47 +0900

    make main function runnable

--- main.c
+++ main.c
@@ -1,22 +1,49 @@
 #include "Analyzer/lexer.h"
 #include "Analyzer/parser.h"
-#include "Analyzer/evaluator.h"
 #include "Utility/debug.h"
+#include "Analyzer/evaluator.h"
+
+void left_case(Either(Token | Node | Signal) either);
 
 int main(int argc, char *argv[]) {
-    //tokenize("1 xor not not 1 or 1");
-    //tokenize("1 or 0 and 0");
-    //tokenize("1 and 0 or 1");
+//    tokenize("1 xor not not 1 or 1");
+//    tokenize("1 or 0 and 0");
+//    tokenize("1 and 0 or 1");
+//"not not not 1 xor (1 or 1)"
+    char string[] = "1 and 0 or not not notnotnot 1";
+    puts(string);
 
-    Token *token = tokenize("not 1 xor (1 or 1)");
-    print_token(token);
+    Either(Token*) either_token = tokenize(string);
+    left_case(either_token);
 
-    Node *node = parse(token);
-    print_node(node);
+    if (is_right(either_token)) {
+        Token *token = (Token *) either_token.right;
+        print_token(token);
 
-    eval(node);
+        Either(Node*) either_node = parse(token);
+        left_case(either_node);
 
-    printf("%u\n", evaluator.signal.value);
+        if (is_right(either_node)) {
+            Node *node = (Node *) either_node.right;
+            print_node(node);
+
+            Either(Signal) either_signal = eval(node);
+            left_case(either_signal);
+
+            if (is_right(either_signal)) {
+                Signal* signal = (Signal *) either_signal.right;
+
+                printf("%u\n", signal->value);
+            }
+        }
+    }
 
     return 0;
+}
+
+void left_case(Either(Token | Node | Signal) either) {
+    if (is_left(either)) {
+        puts(either.left);
+        exit(1);
+    }
 }
